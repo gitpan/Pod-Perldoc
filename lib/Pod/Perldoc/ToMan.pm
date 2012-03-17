a86ed9d8fc43139aedb790eb59ef6307e1fd62bf
Mark Allen <mrallen1@yahoo.com>
2012-03-17 05:02:18 +0000

    Import of MALLEN/Pod-Perldoc-3.16 from CPAN.
    gitpan-cpan-distribution: Pod-Perldoc
    gitpan-cpan-version:      3.16
    gitpan-cpan-path:         MALLEN/Pod-Perldoc-3.16.tar.gz
    gitpan-cpan-author:       MALLEN
    gitpan-cpan-maturity:     released

--- lib/Pod/Perldoc/ToMan.pm
+++ lib/Pod/Perldoc/ToMan.pm
@@ -5,7 +5,7 @@ use warnings;
 use parent qw(Pod::Perldoc::BaseTo);
 
 use vars qw($VERSION);
-$VERSION = '3.15_15';
+$VERSION = '3.16';
 
 use File::Spec::Functions qw(catfile);
 use Pod::Man 2.18;
@@ -199,7 +199,7 @@ sub _have_groff_with_utf8 {
 			 );
 		}
 
-	$version gt $minimum_groff_version;
+	$version ge $minimum_groff_version;
 	}
 
 sub _have_mandoc_with_utf8 {
@@ -224,7 +224,7 @@ sub _have_mandoc_with_utf8 {
 			 );
 		}
 
-	$version gt $minimum_mandoc_version;
+	$version ge $minimum_mandoc_version;
 	}
 
 sub _collect_nroff_switches {
